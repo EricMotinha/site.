985113f467234212c1033228529a6a0e12cb1092
EricMotinha <148996094+EricMotinha@users.noreply.github.com>
2023-10-25 13:15:22 -0300

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Header from "./header";
+
+function App() {
+  // Remover a declaração do componente `Header`
+
+  return (
+    <>
+      <Header />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
